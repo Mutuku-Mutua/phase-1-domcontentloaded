dad3dde73fa3c780fa1aff98ce1d7f85cba9f168
Mutuku Mutua <mutua.mutuku@student.moringaschool.com>
2022-12-14 14:45:08 +0300

    Assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+
+
+document.addEventListener("DOMContentLoaded",()=>{
+    // const pTag = document.querySelector('#text')
+    // console.log(pTag)
+    // pTag.textContent = 'This is really cool!'
+    document.querySelector('#text').textContent = 'This is really cool!'
+
+
+})
